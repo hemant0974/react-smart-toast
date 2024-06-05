d755b1942efcd159fc62f3b5a1c33e364f4aca95
Hemant <hemantjadhav8825@gmail.com>
2024-06-06 00:49:45 +0530

    useSmartToast partially working

--- src/hooks/useSmartToast.tsx
+++ src/hooks/useSmartToast.tsx
@@ -1,9 +1,58 @@
 import React, { useState } from "react";
-import SmartToast, { IToastTypes } from "../components/smartToast";
+import SmartToast, {
+  IToastTypes,
+  IAllVariants,
+} from "../components/smartToast";
+
+interface ISmartToastOptions {
+  type: IAllVariants;
+}
+
+let toastOptions: IToastTypes | null = null;
 
 const useSmartToast = () => {
   const [list, setList] = useState<IToastTypes[]>([]);
+
+  const toastSmart = (title: string, { type }: ISmartToastOptions) => {
+    switch (type) {
+      case "success":
+        toastOptions = {
+          id: list.length + 1,
+          title: "Success",
+          description: "This is a success toast component",
+          variant: "success",
+        };
+        break;
+      case "error":
+        toastOptions = {
+          id: list.length + 1,
+          title: "Danger",
+          description: "This is a danger toast component",
+          variant: "error",
+        };
+        break;
+      case "info":
+        toastOptions = {
+          id: list.length + 1,
+          title: "Info",
+          description: "This is a info toast component",
+          variant: "info",
+        };
+        break;
+      case "warning":
+        toastOptions = {
+          id: list.length + 1,
+          title: "Warning",
+          description: "This is a warning toast component",
+          variant: "warning",
+        };
+        break;
+    }
+    setList([...list, toastOptions]);
+  };
+
   return {
+    toastSmart,
     SmartToastComponent: () => (
       <SmartToast toastlist={list} position="bottom-center" setList={setList} />
     ),
