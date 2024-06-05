8fe0ea00eb000b07e636532f0bdbc3191092c00f
Hemant <hemantjadhav8825@gmail.com>
2024-06-06 01:11:21 +0530

    changes in useSmartToast hook

--- src/hooks/useSmartToast.tsx
+++ src/hooks/useSmartToast.tsx
@@ -6,49 +6,20 @@ import SmartToast, {
 
 interface ISmartToastOptions {
   type: IAllVariants;
+  subDesc?: string;
 }
-
-let toastOptions: IToastTypes | null = null;
 
 const useSmartToast = () => {
   const [list, setList] = useState<IToastTypes[]>([]);
 
-  const toastSmart = (title: string, { type }: ISmartToastOptions) => {
-    switch (type) {
-      case "success":
-        toastOptions = {
-          id: list.length + 1,
-          title: "Success",
-          description: "This is a success toast component",
-          variant: "success",
-        };
-        break;
-      case "error":
-        toastOptions = {
-          id: list.length + 1,
-          title: "Danger",
-          description: "This is a danger toast component",
-          variant: "error",
-        };
-        break;
-      case "info":
-        toastOptions = {
-          id: list.length + 1,
-          title: "Info",
-          description: "This is a info toast component",
-          variant: "info",
-        };
-        break;
-      case "warning":
-        toastOptions = {
-          id: list.length + 1,
-          title: "Warning",
-          description: "This is a warning toast component",
-          variant: "warning",
-        };
-        break;
-    }
-    setList([...list, toastOptions]);
+  const toastSmart = (title: string, { type, subDesc }: ISmartToastOptions) => {
+    const newToastOption = {} as IToastTypes;
+    newToastOption.title = title;
+    newToastOption.variant = type;
+    newToastOption.description = subDesc || "";
+    newToastOption.id = list.length + 1;
+
+    setList([...list, newToastOption]);
   };
 
   return {
