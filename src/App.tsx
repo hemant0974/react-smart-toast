ab2e5664ddc81167284ef4a4468a1cfc8f1c7bfd
Hemant <hemantjadhav8825@gmail.com>
2024-06-05 20:58:20 +0530

    made it working

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,68 @@
+import { useState } from "react";
 import Button from "./components/button";
+import SmartToast from "./components/smartToast";
 import styles from "./app.module.scss";
 
 const variants = ["primary", "success", "warning", "error"] as const;
 
 const App = () => {
+  const [list, setList] = useState<any>([]);
+  let toastProperties = null;
+
+  const showToast = (type: (typeof variants)[number]) => {
+    switch (type) {
+      case "success":
+        toastProperties = {
+          id: list.length + 1,
+          title: "Success",
+          description: "This is a success toast component",
+          backgroundColor: "#5cb85c",
+        };
+        break;
+      case "error":
+        toastProperties = {
+          id: list.length + 1,
+          title: "Danger",
+          description: "This is a danger toast component",
+          backgroundColor: "#d9534f",
+        };
+        break;
+      case "primary":
+        toastProperties = {
+          id: list.length + 1,
+          title: "Info",
+          description: "This is a info toast component",
+          backgroundColor: "#5bc0de",
+        };
+        break;
+      case "warning":
+        toastProperties = {
+          id: list.length + 1,
+          title: "Warning",
+          description: "This is a warning toast component",
+          backgroundColor: "#f0ad4e",
+        };
+        break;
+      default:
+        toastProperties = [];
+    }
+    setList([...list, toastProperties]);
+  };
+
+  const handleBtnClick = (variant: (typeof variants)[number]) => {
+    showToast(variant);
+  };
+
   return (
     <div className={styles.appWrapper}>
       <div className={styles.btnContainer}>
-        {variants.map((variants) => (
-          <Button color={variants}>{variants}</Button>
+        {variants.map((variant) => (
+          <Button onClick={() => handleBtnClick(variant)} color={variant}>
+            {variant}
+          </Button>
         ))}
       </div>
+      <SmartToast toastlist={list} position="buttom-right" setList={setList} />
     </div>
   );
 };
--- src/components/smartToast/index.tsx
+++ src/components/smartToast/index.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useCallback, useEffect } from "react";
+import styles from "./smartToast.module.scss";
 
-const SmartToast = () => {
-  return <div></div>;
+interface ISmartToast {
+  toastlist: any;
+  position: any;
+  setList: any;
+}
+
+const SmartToast = ({ toastlist, position, setList }: ISmartToast) => {
+  const deleteToast = useCallback(
+    (id: any) => {
+      const toastListItem = toastlist.filter((e: any) => e.id !== id);
+      setList(toastListItem);
+    },
+    [toastlist, setList]
+  );
+
+  useEffect(() => {
+    // const interval = setInterval(() => {
+    //   if (toastlist.length) {
+    //     deleteToast(toastlist[0].id);
+    //   }
+    // }, 3000);
+
+    return () => {
+      // clearInterval(interval);
+    };
+  }, [toastlist, deleteToast]);
+
+  return (
+    <div className={`${styles.container} ${styles[position]}`}>
+      {toastlist.map((toast: any, index: number) => (
+        <div
+          key={index}
+          className={`${styles.notification} ${styles.toast} ${styles[position]}`}
+          style={{ backgroundColor: toast.backgroundColor }}
+        >
+          <button onClick={() => deleteToast(toast.id)}>X</button>
+          <div>
+            <p className={styles.title}>{toast.title}</p>
+            <p className={styles.description}>{toast.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SmartToast;
