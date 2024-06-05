bc0a08f63fd6600db3f1889b3df30139a08fff96
Hemant <hemantjadhav8825@gmail.com>
2024-06-05 19:47:34 +0530

    button variants added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import Button from "./components/button";
+
+const flexStyle: React.CSSProperties = {
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const variants = ["primary", "success", "warning", "error"] as const;
+
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div style={flexStyle}>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        {variants.map((variants) => (
+          <Button color={variants}>{variants}</Button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
